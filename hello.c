90ea808369877415cb4d462e5d1dbf8e2523314e
yzheng13 <54691325+yzheng13@users.noreply.github.com>
2019-08-29 19:29:09 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+*This program is for CSCE155E Hack
+ * Author: Yaxuan Zheng, 
+ * Date: 2010/08/29
+ *
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Yaxuan Zheng");
+  printf("Electrical Engineering\n");
+  
+  return 0;
+}
